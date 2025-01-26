9c1eec1f05b450df75e81d3e2ab906664558f6d4
Yuslash <sibikrishna71@gmail.com>
2025-01-26 12:03:42 +0000

    now lets setup the environment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Experience from "./Components/Experiece";
 export default function App() {
 
   return (
-    <div className="">
+    <div className="absoulte top-0 left-0 w-full h-screen ">
       <Experience />
     </div>
   )
--- src/Components/Experiece.jsx
+++ src/Components/Experiece.jsx
@@ -1,6 +1,48 @@
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useEffect } from "react";
+import { useRef } from "react";
+
+function PlaneSurface() {
+  
+  const planeRef = useRef()
+
+  useEffect(() => {
+    
+    if(planeRef.current) {
+      planeRef.current.rotation.x = -Math.PI / 2
+      planeRef.current.position.y = -1
+    }
+
+  }, [planeRef.current])
+
+  return (
+    <mesh ref={planeRef}>
+      <planeGeometry args={[5,5]} />
+      <meshStandardMaterial />
+    </mesh>
+  )
+}
+
+function BoxCube() {
+  return (
+    <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+    </mesh>
+  )
+}
+
 export default function Experience() {
   
   return (
-    <div>how are you this is experience </div>
+    <div className="w-full h-full">
+        <Canvas>
+          <PlaneSurface />
+          <BoxCube />
+          <Environment preset="city" />
+          <OrbitControls />
+        </Canvas>
+    </div>
   )
 }
