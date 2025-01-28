0372f4a4562926f39047c7525293e514791e22d0
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-28 20:53:08 +0530

    ok no2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Events from "./Components/Events";
 import Experience from "./Components/Experiece";
 
 export default function App() {
 
   return (
     <div className="absoulte top-0 left-0 w-full h-screen ">
-      <Experience />
+      <Events />
     </div>
   )
 
--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useMemo } from "react"
+
 export default function Events() {
+
+  // console.log("Hello world")
+  
+  useMemo(() => {
+    console.log("hello woirld")
+   } ,[])
 
   return (
     <>
       <div className="">
+        
       </div>
     </>
   )
