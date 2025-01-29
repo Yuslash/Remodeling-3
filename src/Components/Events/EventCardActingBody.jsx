ef90d5a34375fb0436ffaa1b4588d9470d87190e
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-29 17:06:42 +0530

    created a structure now we are gonna create a data to map

---
+++ src/Components/Events/EventCardActingBody.jsx
@@ -0,0 +1,11 @@
+export default function EventCardActingBody() {
+
+    return (
+        <div className="flex flex-wrap max-w-[1000px] gap-[10px]">
+            <div className="acting-card w-[495px] h-[300px]"></div>
+            <div className="acting-card w-[495px] h-[300px]"></div>
+            <div className="acting-card w-[495px] h-[300px]"></div>
+            <div className="acting-card w-[495px] h-[300px]"></div>
+        </div>
+    )
+}
--- src/Components/Events/EventMainBody.jsx
+++ src/Components/Events/EventMainBody.jsx
@@ -1,7 +1,23 @@
+import EventCardActingBody from "./EventCardActingBody"
+
 export default function EventMainBody() {
 
+    console.log(window.innerWidth * 0.75)
+
     return (
-        <div className="w-full h-full bg-orange-500 flex-1">
+        <div className="h-full bg-orange-500 flex-1 flex ">
+            
+            {/* Cards Container Starts here */}
+                <div  className="acting-body w-[75vw] flex justify-center items-center">
+                    <EventCardActingBody />
+                </div>
+            {/* Cards Container Ends here */}
+
+            {/* Leveling Container Starts here */}
+                <div className="w-[25vw] bg-green-300">
+
+                </div>
+            {/* Leveling Container Ends here */}
         </div>
     )
 }
