b8f452391cb6a529028e2f6b70e8d45f46c09a64
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-30 11:43:21 +0530

    i have removed the Event Progress

--- src/Components/Events/EventCardActingBody.jsx
+++ src/Components/Events/EventCardActingBody.jsx
@@ -3,9 +3,69 @@ import EventSvg from "../EventSvg";
 export default function EventCardActingBody() {
 
     return (
-        <div className="flex flex-wrap gap-[10px] justify-center p-6">
+        <div className="flex flex-wrap max-w-[1050px] gap-[10px] justify-center p-6">
            
             {/* acting card starts here */}
+            <div className="acting-card w-[495px] h-[300px] flex p-[20px] gap[10px]">
+                <EventSvg />
+
+                {/* right side container for content information */}
+                <div className="w-[265px] h-full text-white flex flex-col justify-between pl-[20px]">
+
+                    {/* games names column starts here */}
+                    <div className="flex flex-col">
+                        <div className="game-title">Games Name</div>
+                        <div className="flex gap-[8px]">
+                            <img src="/cert.png" alt="sibi"></img>
+                            <div className="game-value">Certification</div>
+                        </div>
+                    </div>
+                    {/* games names column ends here */}
+
+                    {/* game current level starts here */}
+                    <div className="flex flex-col ">
+                        <div className="game-title">Game Current Level</div>
+                        <div className="game-value">Master RainDrop</div>
+                    </div>
+                    {/* game current level ends here */}
+
+                    {/* Previous Level Starts here */}
+                    <div className="flex gap-[20px]">
+                        
+                        <div className="flex flex-col ">
+                            <div className="game-title">Previous Level</div>
+                            <div className="game-value">Shang Chi</div>
+                        </div>
+
+                        <div className="flex flex-col ">
+                            <div className="game-title">Next Level</div>
+                            <div className="game-value">Jackie Chan</div>
+                        </div>
+                    
+                    </div>
+                    {/* Previous Level ends here */}
+
+                    <div className="flex flex-col ">
+                        <div className="game-title">Total Score</div>
+                        <div className="flex gap-[8px]">
+                            <img src="/badge.png" alt="sco"></img>
+                            <div className="game-value">25739</div>
+                        </div>
+                    </div>
+                    {/* total score ends here */}
+
+                    {/* Select Button Starts here */}
+                    <div onClick={() => console.log('sibikrishna')} className="event-select-btn w-full justify-center flex">
+                        Select
+                    </div>
+                    {/* Select Button Ends here */}
+
+
+                </div>
+            </div>
+            {/* acting card ends here */}
+
+            {/* acting card starts here */}
             <div className="acting-card w-[495px] h-[300px] flex p-[20px] gap-[10px]">
                 <EventSvg />
 
@@ -55,7 +115,68 @@ export default function EventCardActingBody() {
                     {/* total score ends here */}
 
                     {/* Select Button Starts here */}
-                    <button onClick={console.log('sibikrishna')} className="event-select-btn w-full justify-center flex">
+                    <button onClick={() => console.log('sibikrishna')} className="event-select-btn w-full justify-center flex">
+                        Select
+                    </button>
+                    {/* Select Button Ends here */}
+
+
+                </div>
+            </div>
+            {/* acting card ends here */}
+
+
+            {/* acting card starts here */}
+            <div className="acting-card w-[495px] h-[300px] flex p-[20px] gap-[10px]">
+                <EventSvg />
+
+                {/* right side container for content information */}
+                <div className="w-[265px] h-full  text-white flex flex-col justify-between pl-[20px]">
+
+                    {/* games names column starts here */}
+                    <div className="flex flex-col">
+                        <div className="game-title">Games Name</div>
+                        <div className="flex gap-[8px]">
+                            <img src="/cert.png" alt="sibi"></img>
+                            <div className="game-value">Certification</div>
+                        </div>
+                    </div>
+                    {/* games names column ends here */}
+
+                    {/* game current level starts here */}
+                    <div className="flex flex-col ">
+                        <div className="game-title">Game Current Level</div>
+                        <div className="game-value">Master RainDrop</div>
+                    </div>
+                    {/* game current level ends here */}
+
+                    {/* Previous Level Starts here */}
+                    <div className="flex gap-[20px]">
+                        
+                        <div className="flex flex-col ">
+                            <div className="game-title">Previous Level</div>
+                            <div className="game-value">Shang Chi</div>
+                        </div>
+
+                        <div className="flex flex-col ">
+                            <div className="game-title">Next Level</div>
+                            <div className="game-value">Jackie Chan</div>
+                        </div>
+                    
+                    </div>
+                    {/* Previous Level ends here */}
+
+                    <div className="flex flex-col ">
+                        <div className="game-title">Total Score</div>
+                        <div className="flex gap-[8px]">
+                            <img src="/badge.png" alt="sco"></img>
+                            <div className="game-value">25739</div>
+                        </div>
+                    </div>
+                    {/* total score ends here */}
+
+                    {/* Select Button Starts here */}
+                    <button onClick={() => console.log('sibikrishna')} className="event-select-btn w-full justify-center flex">
                         Select
                     </button>
                     {/* Select Button Ends here */}
@@ -115,128 +236,7 @@ export default function EventCardActingBody() {
                     {/* total score ends here */}
 
                     {/* Select Button Starts here */}
-                    <button onClick={console.log('sibikrishna')} className="event-select-btn w-full justify-center flex">
-                        Select
-                    </button>
-                    {/* Select Button Ends here */}
-
-
-                </div>
-            </div>
-            {/* acting card ends here */}
-
-
-            {/* acting card starts here */}
-            <div className="acting-card w-[495px] h-[300px] flex p-[20px] gap-[10px]">
-                <EventSvg />
-
-                {/* right side container for content information */}
-                <div className="w-[265px] h-full  text-white flex flex-col justify-between pl-[20px]">
-
-                    {/* games names column starts here */}
-                    <div className="flex flex-col">
-                        <div className="game-title">Games Name</div>
-                        <div className="flex gap-[8px]">
-                            <img src="/cert.png" alt="sibi"></img>
-                            <div className="game-value">Certification</div>
-                        </div>
-                    </div>
-                    {/* games names column ends here */}
-
-                    {/* game current level starts here */}
-                    <div className="flex flex-col ">
-                        <div className="game-title">Game Current Level</div>
-                        <div className="game-value">Master RainDrop</div>
-                    </div>
-                    {/* game current level ends here */}
-
-                    {/* Previous Level Starts here */}
-                    <div className="flex gap-[20px]">
-                        
-                        <div className="flex flex-col ">
-                            <div className="game-title">Previous Level</div>
-                            <div className="game-value">Shang Chi</div>
-                        </div>
-
-                        <div className="flex flex-col ">
-                            <div className="game-title">Next Level</div>
-                            <div className="game-value">Jackie Chan</div>
-                        </div>
-                    
-                    </div>
-                    {/* Previous Level ends here */}
-
-                    <div className="flex flex-col ">
-                        <div className="game-title">Total Score</div>
-                        <div className="flex gap-[8px]">
-                            <img src="/badge.png" alt="sco"></img>
-                            <div className="game-value">25739</div>
-                        </div>
-                    </div>
-                    {/* total score ends here */}
-
-                    {/* Select Button Starts here */}
-                    <button onClick={console.log('sibikrishna')} className="event-select-btn w-full justify-center flex">
-                        Select
-                    </button>
-                    {/* Select Button Ends here */}
-
-
-                </div>
-            </div>
-            {/* acting card ends here */}
-
-            {/* acting card starts here */}
-            <div className="acting-card w-[495px] h-[300px] flex p-[20px] gap-[10px]">
-                <EventSvg />
-
-                {/* right side container for content information */}
-                <div className="w-[265px] h-full  text-white flex flex-col justify-between pl-[20px]">
-
-                    {/* games names column starts here */}
-                    <div className="flex flex-col">
-                        <div className="game-title">Games Name</div>
-                        <div className="flex gap-[8px]">
-                            <img src="/cert.png" alt="sibi"></img>
-                            <div className="game-value">Certification</div>
-                        </div>
-                    </div>
-                    {/* games names column ends here */}
-
-                    {/* game current level starts here */}
-                    <div className="flex flex-col ">
-                        <div className="game-title">Game Current Level</div>
-                        <div className="game-value">Master RainDrop</div>
-                    </div>
-                    {/* game current level ends here */}
-
-                    {/* Previous Level Starts here */}
-                    <div className="flex gap-[20px]">
-                        
-                        <div className="flex flex-col ">
-                            <div className="game-title">Previous Level</div>
-                            <div className="game-value">Shang Chi</div>
-                        </div>
-
-                        <div className="flex flex-col ">
-                            <div className="game-title">Next Level</div>
-                            <div className="game-value">Jackie Chan</div>
-                        </div>
-                    
-                    </div>
-                    {/* Previous Level ends here */}
-
-                    <div className="flex flex-col ">
-                        <div className="game-title">Total Score</div>
-                        <div className="flex gap-[8px]">
-                            <img src="/badge.png" alt="sco"></img>
-                            <div className="game-value">25739</div>
-                        </div>
-                    </div>
-                    {/* total score ends here */}
-
-                    {/* Select Button Starts here */}
-                    <button onClick={console.log('sibikrishna')} className="event-select-btn w-full justify-center flex">
+                    <button onClick={() => console.log('sibikrishna')} className="event-select-btn w-full justify-center flex">
                         Select
                     </button>
                     {/* Select Button Ends here */}
--- src/Components/Events/EventMainBody.jsx
+++ src/Components/Events/EventMainBody.jsx
@@ -2,21 +2,19 @@ import EventCardActingBody from "./EventCardActingBody"
 
 export default function EventMainBody() {
 
-    console.log(window.innerWidth * 0.75)
-
     return (
         <div className="h-full flex-1 flex ">
             
             {/* Cards Container Starts here */}
-                <div  className="acting-body w-[75vw] flex justify-center items-center">
+                <div  className="acting-body w-full flex justify-center items-center">
                     <EventCardActingBody />
                 </div>
             {/* Cards Container Ends here */}
 
             {/* Leveling Container Starts here */}
-                <div className="w-[25vw] bg-green-300">
-
-                </div>
+                {/* <div className="w-[25vw] bg-green-300">
+                    not developed yuet
+                </div> */}
             {/* Leveling Container Ends here */}
         </div>
     )
