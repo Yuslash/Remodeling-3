abc03369777c81eb5510206afef2506414fafbb9
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-31 00:24:22 +0530

    i think i should try the svg and use that data to change the progress thing

--- src/Components/EarTesting/EarComplete.jsx
+++ src/Components/EarTesting/EarComplete.jsx
@@ -4,7 +4,9 @@ export default function EarComplete() {
 
     return (
         <div className="text-white w-full h-full bg-yellow-200 flex justify-center items-center">
-            <EarSvg />
+            <div className="shapeofthe">
+                <div className="innershapeofthe"></div>
+            </div>
         </div>
     )
 }
