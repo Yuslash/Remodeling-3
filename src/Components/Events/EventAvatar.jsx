2c22cce70869de9a96b51c627e6bb47471859179
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-01-31 20:35:36 +0530

    billie jean not my lover

--- src/Components/Events/EventAvatar.jsx
+++ src/Components/Events/EventAvatar.jsx
@@ -1,4 +1,7 @@
-<svg width="24" height="27" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+export default function EventAvatar() {
+
+    return (
+        <svg width="24" height="27" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g id="target">
 <g id="leg">
 <g id="Group 224">
@@ -17,24 +20,24 @@
 </g>
 </g>
 <g id="lefthand">
-<path id="downhandbody" d="M1.59961 14.697L3.91961 16.2437V26.297C3.91961 26.297 3.53294 26.297 2.8885 25.7815C2.24405 25.2659 1.7285 24.4926 1.7285 24.4926L1.59961 14.697Z" fill="url(#paint4_linear_233_62)" stroke="black" strokeWidth="0.1"/>
+<path id="downhandbody" d="M1.59961 14.697L3.91961 16.2437V26.297C3.91961 26.297 3.53294 26.297 2.8885 25.7815C2.24405 25.2659 1.7285 24.4926 1.7285 24.4926L1.59961 14.697Z" fill="url(#paint4_linear_233_62)" stroke="black" stroke-width="0.1"/>
 <g id="handear">
 <g filter="url(#filter0_i_233_62)">
 <path d="M2.11621 23.7193V16.7593L3.01843 18.0482V24.6215C3.01843 24.6215 2.56499 24.4577 2.37399 24.2348C2.22749 24.0639 2.11621 23.7193 2.11621 23.7193Z" fill="url(#paint5_linear_233_62)"/>
 </g>
-<path d="M2.11621 23.7193V16.7593L3.01843 18.0482V24.6215C3.01843 24.6215 2.56499 24.4577 2.37399 24.2348C2.22749 24.0639 2.11621 23.7193 2.11621 23.7193Z" stroke="#1F1226" strokeWidth="0.1"/>
+<path d="M2.11621 23.7193V16.7593L3.01843 18.0482V24.6215C3.01843 24.6215 2.56499 24.4577 2.37399 24.2348C2.22749 24.0639 2.11621 23.7193 2.11621 23.7193Z" stroke="#1F1226" stroke-width="0.1"/>
 </g>
-<path id="downhandout" d="M2.37294 16.1148C1.85739 15.4704 1.59961 14.697 1.59961 14.697L3.91961 16.2437V26.297L3.40405 26.1157V17.2748C3.40405 17.2748 2.8885 16.7592 2.37294 16.1148Z" fill="url(#paint6_linear_233_62)" stroke="#242843" strokeWidth="0.1"/>
+<path id="downhandout" d="M2.37294 16.1148C1.85739 15.4704 1.59961 14.697 1.59961 14.697L3.91961 16.2437V26.297L3.40405 26.1157V17.2748C3.40405 17.2748 2.8885 16.7592 2.37294 16.1148Z" fill="url(#paint6_linear_233_62)" stroke="#242843" stroke-width="0.1"/>
 </g>
 <g id="righthand">
-<path id="downhandbody_2" d="M22.7207 14.697L20.4007 16.2437V26.297C20.4007 26.297 20.7874 26.297 21.4318 25.7815C22.0763 25.2659 22.5918 24.4926 22.5918 24.4926L22.7207 14.697Z" fill="url(#paint7_linear_233_62)" stroke="black" strokeWidth="0.1"/>
+<path id="downhandbody_2" d="M22.7207 14.697L20.4007 16.2437V26.297C20.4007 26.297 20.7874 26.297 21.4318 25.7815C22.0763 25.2659 22.5918 24.4926 22.5918 24.4926L22.7207 14.697Z" fill="url(#paint7_linear_233_62)" stroke="black" stroke-width="0.1"/>
 <g id="handear_2">
 <g filter="url(#filter1_i_233_62)">
 <path d="M22.2041 23.7193V16.7593L21.3019 18.0482V24.6215C21.3019 24.6215 21.7553 24.4577 21.9463 24.2348C22.0928 24.0639 22.2041 23.7193 22.2041 23.7193Z" fill="url(#paint8_linear_233_62)"/>
 </g>
-<path d="M22.2041 23.7193V16.7593L21.3019 18.0482V24.6215C21.3019 24.6215 21.7553 24.4577 21.9463 24.2348C22.0928 24.0639 22.2041 23.7193 22.2041 23.7193Z" stroke="#1F1226" strokeWidth="0.1"/>
+<path d="M22.2041 23.7193V16.7593L21.3019 18.0482V24.6215C21.3019 24.6215 21.7553 24.4577 21.9463 24.2348C22.0928 24.0639 22.2041 23.7193 22.2041 23.7193Z" stroke="#1F1226" stroke-width="0.1"/>
 </g>
-<path id="downhandout_2" d="M21.9474 16.1148C22.4629 15.4704 22.7207 14.697 22.7207 14.697L20.4007 16.2437V26.297L20.9163 26.1157V17.2748C20.9163 17.2748 21.4318 16.7592 21.9474 16.1148Z" fill="url(#paint9_linear_233_62)" stroke="#242843" strokeWidth="0.1"/>
+<path id="downhandout_2" d="M21.9474 16.1148C22.4629 15.4704 22.7207 14.697 22.7207 14.697L20.4007 16.2437V26.297L20.9163 26.1157V17.2748C20.9163 17.2748 21.4318 16.7592 21.9474 16.1148Z" fill="url(#paint9_linear_233_62)" stroke="#242843" stroke-width="0.1"/>
 </g>
 <g id="head">
 <g id="Group 224_2">
@@ -53,32 +56,32 @@
 </g>
 </g>
 <g id="avatar">
-<rect width="17.08" height="17.08" transform="translate(3.59961 5.89697)" fill="white" fillOpacity="0.2"/>
+<image href="/theguy.png" width="17.08" height="17.08" x="3.59961" y="5.89697" fill="white" fill-opacity="0.2"/>
 </g>
 <g id="earleft">
-<path id="ear" d="M5.2002 5.69702H12.2002L8.2002 5.89702L3.8002 10.697V14.097L1.2002 8.49702V0.697021L5.2002 5.69702Z" fill="url(#paint14_linear_233_62)" stroke="#2A2647" strokeWidth="0.1"/>
+<path id="ear" d="M5.2002 5.69702H12.2002L8.2002 5.89702L3.8002 10.697V14.097L1.2002 8.49702V0.697021L5.2002 5.69702Z" fill="url(#paint14_linear_233_62)" stroke="#2A2647" stroke-width="0.1"/>
 <g id="inner ear">
 <g filter="url(#filter2_i_233_62)">
 <path d="M1.7998 8.24556V3.29712L3.5998 5.49712V11.2971L1.7998 8.24556Z" fill="url(#paint15_radial_233_62)"/>
 </g>
-<path d="M1.7998 8.24556V3.29712L3.5998 5.49712V11.2971L1.7998 8.24556Z" stroke="#2B2644" strokeWidth="0.1"/>
+<path d="M1.7998 8.24556V3.29712L3.5998 5.49712V11.2971L1.7998 8.24556Z" stroke="#2B2644" stroke-width="0.1"/>
 </g>
-<path id="outerar" d="M6.60019 5.09702L1.2002 0.697021L5.2002 5.69702H9.0002L6.60019 5.09702Z" fill="url(#paint16_linear_233_62)" stroke="#2C2947" strokeWidth="0.1"/>
+<path id="outerar" d="M6.60019 5.09702L1.2002 0.697021L5.2002 5.69702H9.0002L6.60019 5.09702Z" fill="url(#paint16_linear_233_62)" stroke="#2C2947" stroke-width="0.1"/>
 </g>
 <g id="earright">
-<path id="ear_2" d="M19 5.69702H12L16 5.89702L20.4 10.697V14.097L23 8.49702V0.697021L19 5.69702Z" fill="url(#paint17_linear_233_62)" stroke="#2A2647" strokeWidth="0.1"/>
+<path id="ear_2" d="M19 5.69702H12L16 5.89702L20.4 10.697V14.097L23 8.49702V0.697021L19 5.69702Z" fill="url(#paint17_linear_233_62)" stroke="#2A2647" stroke-width="0.1"/>
 <g id="inner ear_2">
 <g filter="url(#filter3_i_233_62)">
 <path d="M22.4004 8.24556V3.29712L20.6004 5.49712V11.2971L22.4004 8.24556Z" fill="url(#paint18_radial_233_62)"/>
 </g>
-<path d="M22.4004 8.24556V3.29712L20.6004 5.49712V11.2971L22.4004 8.24556Z" stroke="#2B2644" strokeWidth="0.1"/>
+<path d="M22.4004 8.24556V3.29712L20.6004 5.49712V11.2971L22.4004 8.24556Z" stroke="#2B2644" stroke-width="0.1"/>
 </g>
-<path id="outerar_2" d="M17.6 5.09702L23 0.697021L19 5.69702H15.2L17.6 5.09702Z" fill="url(#paint19_linear_233_62)" stroke="#2C2947" strokeWidth="0.1"/>
+<path id="outerar_2" d="M17.6 5.09702L23 0.697021L19 5.69702H15.2L17.6 5.09702Z" fill="url(#paint19_linear_233_62)" stroke="#2C2947" stroke-width="0.1"/>
 </g>
 </g>
 <defs>
-<filter id="filter0_i_233_62" x="2.06641" y="16.6006" width="3.00195" height="8.09204" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-<feFlood floodOpacity="0" result="BackgroundImageFix"/>
+<filter id="filter0_i_233_62" x="2.06641" y="16.6006" width="3.00195" height="8.09204" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
 <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
 <feOffset dx="2"/>
@@ -87,8 +90,8 @@
 <feColorMatrix type="matrix" values="0 0 0 0 0.0977124 0 0 0 0 0.913725 0 0 0 0 0.0235294 0 0 0 1 0"/>
 <feBlend mode="normal" in2="shape" result="effect1_innerShadow_233_62"/>
 </filter>
-<filter id="filter1_i_233_62" x="21.252" y="16.6006" width="3.00195" height="8.09204" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-<feFlood floodOpacity="0" result="BackgroundImageFix"/>
+<filter id="filter1_i_233_62" x="21.252" y="16.6006" width="3.00195" height="8.09204" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
 <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
 <feOffset dx="2"/>
@@ -97,8 +100,8 @@
 <feColorMatrix type="matrix" values="0 0 0 0 0.0980392 0 0 0 0 0.92549 0 0 0 0 0.0392157 0 0 0 1 0"/>
 <feBlend mode="normal" in2="shape" result="effect1_innerShadow_233_62"/>
 </filter>
-<filter id="filter2_i_233_62" x="1.75" y="3.15698" width="2.89941" height="10.1232" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-<feFlood floodOpacity="0" result="BackgroundImageFix"/>
+<filter id="filter2_i_233_62" x="1.75" y="3.15698" width="2.89941" height="10.1232" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
 <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
 <feOffset dx="1" dy="2"/>
@@ -107,8 +110,8 @@
 <feColorMatrix type="matrix" values="0 0 0 0 0.117647 0 0 0 0 0.941176 0 0 0 0 0.0509804 0 0 0 1 0"/>
 <feBlend mode="normal" in2="shape" result="effect1_innerShadow_233_62"/>
 </filter>
-<filter id="filter3_i_233_62" x="20.5508" y="3.15698" width="2.29941" height="8.32324" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-<feFlood floodOpacity="0" result="BackgroundImageFix"/>
+<filter id="filter3_i_233_62" x="20.5508" y="3.15698" width="2.29941" height="8.32324" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
 <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
 <feOffset dx="0.4"/>
@@ -118,84 +121,87 @@
 <feBlend mode="normal" in2="shape" result="effect1_innerShadow_233_62"/>
 </filter>
 <linearGradient id="paint0_linear_233_62" x1="8.5459" y1="23.5454" x2="8.5459" y2="22.6969" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1EF00D"/>
-<stop offset="1" stopColor="#2CF917"/>
+<stop stop-color="#1EF00D"/>
+<stop offset="1" stop-color="#2CF917"/>
 </linearGradient>
 <linearGradient id="paint1_linear_233_62" x1="8.5459" y1="23.5454" x2="8.5459" y2="22.6969" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1EF00D"/>
-<stop offset="1" stopColor="#2CF917"/>
+<stop stop-color="#1EF00D"/>
+<stop offset="1" stop-color="#2CF917"/>
 </linearGradient>
 <linearGradient id="paint2_linear_233_62" x1="15.4541" y1="23.5454" x2="15.4541" y2="22.6969" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1EF00D"/>
-<stop offset="1" stopColor="#2CF917"/>
+<stop stop-color="#1EF00D"/>
+<stop offset="1" stop-color="#2CF917"/>
 </linearGradient>
 <linearGradient id="paint3_linear_233_62" x1="15.4541" y1="23.5454" x2="15.4541" y2="22.6969" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1EF00D"/>
-<stop offset="1" stopColor="#2CF917"/>
+<stop stop-color="#1EF00D"/>
+<stop offset="1" stop-color="#2CF917"/>
 </linearGradient>
 <linearGradient id="paint4_linear_233_62" x1="2.75961" y1="14.697" x2="2.75961" y2="26.297" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1D1A2D"/>
-<stop offset="1" stopColor="#0F0718"/>
+<stop stop-color="#1D1A2D"/>
+<stop offset="1" stop-color="#0F0718"/>
 </linearGradient>
 <linearGradient id="paint5_linear_233_62" x1="2.56732" y1="16.7593" x2="2.56732" y2="24.6215" gradientUnits="userSpaceOnUse">
-<stop stopColor="#0AFE1A"/>
-<stop offset="1" stopColor="#22FE29"/>
+<stop stop-color="#0AFE1A"/>
+<stop offset="1" stop-color="#22FE29"/>
 </linearGradient>
 <linearGradient id="paint6_linear_233_62" x1="2.75961" y1="14.697" x2="2.75961" y2="26.297" gradientUnits="userSpaceOnUse">
-<stop stopColor="#26223E"/>
-<stop offset="1" stopColor="#130F21"/>
+<stop stop-color="#26223E"/>
+<stop offset="1" stop-color="#130F21"/>
 </linearGradient>
 <linearGradient id="paint7_linear_233_62" x1="21.5607" y1="14.697" x2="21.5607" y2="26.297" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1D1A2D"/>
-<stop offset="1" stopColor="#0F0718"/>
+<stop stop-color="#1D1A2D"/>
+<stop offset="1" stop-color="#0F0718"/>
 </linearGradient>
 <linearGradient id="paint8_linear_233_62" x1="21.753" y1="16.7593" x2="21.753" y2="24.6215" gradientUnits="userSpaceOnUse">
-<stop stopColor="#FE2E0A"/>
-<stop offset="1" stopColor="#FE6222"/>
+<stop stop-color="#FE2E0A"/>
+<stop offset="1" stop-color="#FE6222"/>
 </linearGradient>
 <linearGradient id="paint9_linear_233_62" x1="21.5607" y1="14.697" x2="21.5607" y2="26.297" gradientUnits="userSpaceOnUse">
-<stop stopColor="#26223E"/>
-<stop offset="1" stopColor="#130F21"/>
+<stop stop-color="#26223E"/>
+<stop offset="1" stop-color="#130F21"/>
 </linearGradient>
 <linearGradient id="paint10_linear_233_62" x1="9.37839" y1="4.9397" x2="9.37839" y2="5.78818" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1EF00D"/>
-<stop offset="1" stopColor="#2CF917"/>
+<stop stop-color="#1EF00D"/>
+<stop offset="1" stop-color="#2CF917"/>
 </linearGradient>
 <linearGradient id="paint11_linear_233_62" x1="9.37839" y1="4.9397" x2="9.37839" y2="5.78818" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1EF00D"/>
-<stop offset="1" stopColor="#2CF917"/>
+<stop stop-color="#1EF00D"/>
+<stop offset="1" stop-color="#2CF917"/>
 </linearGradient>
 <linearGradient id="paint12_linear_233_62" x1="15.0201" y1="4.9397" x2="15.0201" y2="5.78818" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1EF00D"/>
-<stop offset="1" stopColor="#2CF917"/>
+<stop stop-color="#1EF00D"/>
+<stop offset="1" stop-color="#2CF917"/>
 </linearGradient>
 <linearGradient id="paint13_linear_233_62" x1="15.0201" y1="4.9397" x2="15.0201" y2="5.78818" gradientUnits="userSpaceOnUse">
-<stop stopColor="#1EF00D"/>
-<stop offset="1" stopColor="#2CF917"/>
+<stop stop-color="#1EF00D"/>
+<stop offset="1" stop-color="#2CF917"/>
 </linearGradient>
 <linearGradient id="paint14_linear_233_62" x1="1.2002" y1="1.29702" x2="4.6002" y2="10.697" gradientUnits="userSpaceOnUse">
-<stop stopColor="#18162B"/>
-<stop offset="1" stopColor="#1C1B2F"/>
+<stop stop-color="#18162B"/>
+<stop offset="1" stop-color="#1C1B2F"/>
 </linearGradient>
 <radialGradient id="paint15_radial_233_62" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(2.6998 7.29712) rotate(90) scale(4 0.9)">
-<stop stopColor="#51FF25"/>
-<stop offset="1" stopColor="#09FF11"/>
+<stop stop-color="#51FF25"/>
+<stop offset="1" stop-color="#09FF11"/>
 </radialGradient>
 <linearGradient id="paint16_linear_233_62" x1="1.4002" y1="1.09702" x2="8.8002" y2="5.69702" gradientUnits="userSpaceOnUse">
-<stop stopColor="#2B1E3C"/>
-<stop offset="1" stopColor="#1F0A3C"/>
+<stop stop-color="#2B1E3C"/>
+<stop offset="1" stop-color="#1F0A3C"/>
 </linearGradient>
 <linearGradient id="paint17_linear_233_62" x1="23" y1="1.29702" x2="19.6" y2="10.697" gradientUnits="userSpaceOnUse">
-<stop stopColor="#18162B"/>
-<stop offset="1" stopColor="#1C1B2F"/>
+<stop stop-color="#18162B"/>
+<stop offset="1" stop-color="#1C1B2F"/>
 </linearGradient>
 <radialGradient id="paint18_radial_233_62" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(21.5004 7.29712) rotate(90) scale(4 0.9)">
-<stop stopColor="#51FF25"/>
-<stop offset="1" stopColor="#09FF11"/>
+<stop stop-color="#51FF25"/>
+<stop offset="1" stop-color="#09FF11"/>
 </radialGradient>
 <linearGradient id="paint19_linear_233_62" x1="22.8" y1="1.09702" x2="15.4" y2="5.69702" gradientUnits="userSpaceOnUse">
-<stop stopColor="#2B1E3C"/>
-<stop offset="1" stopColor="#1F0A3C"/>
+<stop stop-color="#2B1E3C"/>
+<stop offset="1" stop-color="#1F0A3C"/>
 </linearGradient>
 </defs>
 </svg>
+
+    )
+}
--- src/Components/Events/MiniLeaderBoard.jsx
+++ src/Components/Events/MiniLeaderBoard.jsx
@@ -1,5 +1,119 @@
+import EarSvg from "../EarTesting/EarSvg";
+import EventAvatar from "./EventAvatar";
+import EventAvatarRed from "./EventAvatarRed";
+
 export default function MiniLeaderBoard() {
     return (
-        <div className="text-white">LeaderBoard</div>
+        <div className="leaderboard-container w-full h-full flex flex-col items-center p-[15px]">
+            <div className="leaderboard-title"></div>
+                <div>LeaderBoard</div>
+                <div className="minileader-content overflow-y-auto w-full bg-black flex flex-col gap-4">
+
+                    {/* the title row starts here  */}
+                    <div className="content-row justify-between px-4 flex text-white py-[20px]">
+                        <div className="first-bench flex gap-4 ">
+                            <div>S.No</div>
+                            <div>Badge</div>
+                            <div>Player</div>
+                        </div>
+                        <div className="score-bench">Score</div>
+                    </div>
+                    {/* the title row starts here  */}
+                
+                <div className="playerslist-column flex flex-col gap-4 h-[390px] overflow-y-auto scrollbar-hidden">
+        
+                
+                {/* player list startin here */}
+                <div className="player-row flex px-4 text-white  rounded-xl items-center justify-between py-[16px]">
+                <div className="flex gap-[35px] items-center ">
+                    <div>253</div>
+                    <img className="w-[24px]" src="/badgecrown.png" />
+                    <div className="flex gap-[5px] items-center">
+                    <EventAvatar />
+                    <div>@Sibikrishna</div>
+                    </div>
+                </div>
+                <div>26578</div>
+                </div>
+                {/* player list ending here */}
+
+
+                  {/* player list startin here */}
+                <div className="player-row flex px-4 text-white rounded-xl items-center justify-between py-[16px]">
+                <div className="flex gap-[35px] items-center ">
+                    <div>253</div>
+                    <img className="w-[24px]" src="/badgecrown.png" />
+                    <div className="flex gap-[5px] items-center">
+                    <EventAvatarRed />
+                    <div>@Yuslash Crimson</div>
+                    </div>
+                </div>
+                <div>26578</div>
+                </div>
+                {/* player list ending here */}
+
+                 {/* player list startin here */}
+                 <div className="player-row flex px-4 text-white bg-[#132702] rounded-xl items-center justify-between py-[16px]">
+                <div className="flex gap-[35px] items-center ">
+                    <div>253</div>
+                    <img className="w-[24px]" src="/badgecrown.png" />
+                    <div className="flex gap-[5px] items-center">
+                    <EventAvatar />
+                    <div>@WatchingHere</div>
+                    </div>
+                </div>
+                <div>26578</div>
+                </div>
+                {/* player list ending here */}
+
+                 {/* player list startin here */}
+                 <div className="player-row flex px-4 text-white rounded-xl items-center justify-between py-[16px]">
+                <div className="flex gap-[35px] items-center ">
+                    <div>253</div>
+                    <img className="w-[24px]" src="/badgecrown.png" />
+                    <div className="flex gap-[5px] items-center">
+                    <EventAvatarRed />
+                    <div>@Crimson Connor</div>
+                    </div>
+                </div>
+                <div>26578</div>
+                </div>
+                {/* player list ending here */}
+
+
+                 {/* player list startin here */}
+                 <div className="player-row flex px-4 text-white rounded-xl items-center justify-between py-[16px]">
+                <div className="flex gap-[35px] items-center ">
+                    <div>253</div>
+                    <img className="w-[24px]" src="/badgecrown.png" />
+                    <div className="flex gap-[5px] items-center">
+                    <EventAvatar />
+                    <div>@Jhon Snow</div>
+                    </div>
+                </div>
+                <div>26578</div>
+                </div>
+                {/* player list ending here */}
+
+                 {/* player list startin here */}
+                 <div className="player-row flex px-4 text-white rounded-xl items-center justify-between py-[16px]">
+                <div className="flex gap-[35px] items-center ">
+                    <div>253</div>
+                    <img className="w-[24px]" src="/badgecrown.png" />
+                    <div className="flex gap-[5px] items-center">
+                    <EventAvatarRed />
+                    <div>@Sibikrishna</div>
+                    </div>
+                </div>
+                <div>26578</div>
+                </div>
+                {/* player list ending here */}
+
+                </div>
+                {/* playerlist-colmn list ending here */}
+
+
+                </div>
+        </div>
     )
 }
