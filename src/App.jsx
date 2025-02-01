ac10847e855e16b9f2aeb3b500d6be40fc461a9e
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-02-01 17:11:59 +0530

    why its not working why

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import EarComplete from "./Components/EarTesting/EarComplete";
 import Events from "./Components/Events/Events";
 import Experience from "./Components/Experiece";
+import TestingOver from "./Waves/TestingOver";
 
 export default function App() {
 
   return (
     <div className="absoulte top-0 left-0 w-full h-screen ">
-      <Events />
+      <TestingOver />
     </div>
   )
 
---
+++ src/Waves/TestingOver.jsx
@@ -0,0 +1,12 @@
+import './Wave.css'
+
+export default function TestingOver() {
+
+    return (
+       <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
+            <div className="cube1">
+                <div className="cube2"></div>
+            </div>
+       </div>
+    )
+}
