0275a27278bb3cecad4f2c9bfb410a6849c7ae94
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2025-02-02 16:26:24 +0530

    how are you

--- src/Waves/TestingOver.jsx
+++ src/Waves/TestingOver.jsx
@@ -4,9 +4,7 @@ export default function TestingOver() {
 
     return (
        <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
-            <div className="cube1">
-                <div className="cube2"></div>
-            </div>
+            <div className="cube1"></div>
        </div>
     )
 }
